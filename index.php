431d6da00cd10020d03b4af8a8a8eb1069253ba4
andrea8980 <andrea@casaloda.it>
2026-07-31 19:05:01 +0200

    Refactor code structure for improved readability and maintainability

--- index.php
+++ index.php
@@ -1,52 +1,51 @@
 <?php
-
 $dischi = [
     [
-        "title" => "New Jersey",
-        "author" => "Bon Jovi",
-        "year" => 1988,
-        "poster" => "https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg",
-        "genre" => "Rock"
+        "titolo" => "The Dark Side of the Moon",
+        "artista" => "Pink Floyd",
+        "cover" => "https://placedog.net/500/500?random=1",
+        "anno" => 1973,
+        "genere" => "Progressive Rock"
     ],
     [
-        "title" => "Live at Wembley 86",
-        "author" => "Queen",
-        "year" => 1992,
-        "poster" => "https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg",
-        "genre" => "Pop"
+        "titolo" => "Live at Wembley 86",
+        "artista" => "Queen",
+        "anno" => 1992,
+        "cover" => "https://placedog.net/500/500?random=2",
+        "genere" => "Rock"
+
     ],
     [
-        "title" => "Ten's Summoner's Tales",
-        "author" => "Sting",
-        "year" => 1993,
-        "poster" => "https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg",
-        "genre" => "Pop"
+        "titolo" => "Ten's Summoner's Tales",
+        "artista" => "Sting",
+        "anno" => 1993,
+        "cover" => "https://placedog.net/500/500?random=3",
+        "genere" => "Pop Rock"
+
     ],
     [
-        "title" => "Steve Gadd band",
-        "author" => "Steve Gadd Band",
-        "year" => 2018,
-        "poster" => "https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg",
-        "genre" => "Jazz"
+        "titolo" => "Steve Gadd Band",
+        "artista" => "Steve Gadd Band",
+        "anno" => 2018,
+        "cover" => "https://placedog.net/500/500?random=4",
+        "genere" => "Jazz"
     ],
     [
-        "title" => "'Brave new World'",
-        "author" => "Iron Maiden",
-        "year" => 2000,
-        "poster" => "https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg",
-        "genre" => "Metal"
+        "titolo" => "Brave new World",
+        "artista" => "Iron Maiden",
+        "anno" => 2000,
+        "cover" => "https://placedog.net/500/500?random=5",
+        "genere" => "Heavy Metal"
     ],
     [
-        "title" => "'One more car, one more rider'",
-        "author" => "'Eric Clapton'",
-        "year" => 2002,
-        "poster" => "'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'",
-        "genre" => "'Rock'"
+        "titolo" => "One more car, one more rider",
+        "artista" => "Eric Clapton",
+        "anno" => 2002,
+        "cover" => "https://placedog.net/500/500?random=6",
+        "genere" => "Rock"
     ]
 ];
-
 ?>
-
 
 
 <!DOCTYPE html>
@@ -56,16 +55,42 @@ $dischi = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>php-dischi</h1>
 
-    <?php foreach ($dischi as $disco): ?>
+    <div class="container border border-primary mt-2 mx-auto bg-primary-subtle">
+        <nav class="navbar bg-body-tertiary mt-2">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="https://placedog.net/360/480/pixelate" alt="Bootstrap" width="30" height="24">
+                </a>
+            </div>
+        </nav>
+
+        <h1 class="text-center m-2">php-dischi</h1>
+<div class="container d-flex flex-wrap justify-content-center gap-2 pb-3">
+        <?php foreach ($dischi as $disco => $value) {
+
+echo "<div class='card' style='width: 18rem;'>
+  <img src='{$value['cover']}' class='card-img-top' alt='{$value['titolo']}'>
+  <div class='card-body'>
+    <p class='card-text'>
+        Titolo: {$value['titolo']}<br>
+        Artista: {$value['artista']}<br>
+        Anno: {$value['anno']}<br>
+        Genere: {$value['genere']}
+    </p>
+  </div>
+</div>";
 
 
-    <?php endforeach; ?>
+        } ?>
+    </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
 </body>
 
 </html>

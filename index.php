1810aa1e30d297af4cba025c7cf0b21d82066980
andrea8980 <andrea@casaloda.it>
2026-07-31 18:39:12 +0200

    Add music album data and setup package management

--- index.php
+++ index.php
@@ -1,18 +1,71 @@
+<?php
+
+$dischi = [
+    [
+        "title" => "New Jersey",
+        "author" => "Bon Jovi",
+        "year" => 1988,
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg",
+        "genre" => "Rock"
+    ],
+    [
+        "title" => "Live at Wembley 86",
+        "author" => "Queen",
+        "year" => 1992,
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg",
+        "genre" => "Pop"
+    ],
+    [
+        "title" => "Ten's Summoner's Tales",
+        "author" => "Sting",
+        "year" => 1993,
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg",
+        "genre" => "Pop"
+    ],
+    [
+        "title" => "Steve Gadd band",
+        "author" => "Steve Gadd Band",
+        "year" => 2018,
+        "poster" => "https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg",
+        "genre" => "Jazz"
+    ],
+    [
+        "title" => "'Brave new World'",
+        "author" => "Iron Maiden",
+        "year" => 2000,
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg",
+        "genre" => "Metal"
+    ],
+    [
+        "title" => "'One more car, one more rider'",
+        "author" => "'Eric Clapton'",
+        "year" => 2002,
+        "poster" => "'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'",
+        "genre" => "'Rock'"
+    ]
+];
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>php-dischi</h1>
 
-<?php
+    <?php foreach ($dischi as $disco): ?>
 
 
-
-?>
+    <?php endforeach; ?>
 
 </body>
+
 </html>
